a1efe254022ef97d4bb866325c009d25b654830c
mont <mont@mont-PC>
2017-05-30 22:19:12 +0100

    comments comments and comments

--- src/concertTicket/artist/Artist.java
+++ src/concertTicket/artist/Artist.java
@@ -4,20 +4,22 @@ package concertTicket.artist;
 import java.util.Iterator;
 
 /**
- *
+ * Artist interface
+ * Represent an artist with a name and a collection of albums
+ * 
  * @author Joao Monteiro 51105, Diogo Tavares 50309
  */
 public interface Artist {
 
-	/**
-	 * Get method for the artist's name
-	 * @return the artist's name given when the object is created
-	 */
+    /**
+     * 
+     * @return artist name
+     */
     String getName();
     
     /**
-     * To return a list of the artist's albums
-     * @return List of album titles (Strings), given when the object is created
+     * 
+     * @return iterator containing all albums of this artist
      */
     Iterator<String> getAlbums();
     
