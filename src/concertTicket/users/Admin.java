a3b64d5365f6aff28ecc02f224c50a4b7521c5e0
Utilizador <Utilizador@Utilizador-PC.lan>
2017-05-30 20:57:49 +0100

    commenting..

--- src/concertTicket/users/Admin.java
+++ src/concertTicket/users/Admin.java
@@ -2,7 +2,7 @@
 package concertTicket.users;
 
 /**
- *
+ * 
  * @author Joao Monteiro 51105, Diogo Tavares 50309
  */
 public interface Admin {
--- src/concertTicket/users/User.java
+++ src/concertTicket/users/User.java
@@ -2,12 +2,22 @@
 package concertTicket.users;
 
 /**
- *
+ * Users can log in and register in the FindConcertTicket application, and once they're logged in, they can call every function.
+ * Known subclasses are Client and Admin, which have different priviledges.
  * @author Joao Monteiro 51105, Diogo Tavares 50309
  */
 public interface User {
     
+	/**
+	 * Get method for the user's email
+	 * @return email, which is set in the constructor
+	 */
     String getEmail();
+    
+    /**
+     * Get method for the user's password
+     * @return password, which is set in the constructor
+     */
     String getPassword();
 
 }
