579874b8f535cdd53edef0592552625ba553c8ec
Utilizador <Utilizador@Utilizador-PC.lan>
2017-05-30 22:23:37 +0100

    comments

--- src/concertTicket/users/Admin.java
+++ src/concertTicket/users/Admin.java
@@ -2,7 +2,8 @@
 package concertTicket.users;
 
 /**
- *
+ * Admin is a subclass of User and can call methods in the FindConcertTicket application which deal 
+ * with adding artists and events into the database.
  * @author Joao Monteiro 51105, Diogo Tavares 50309
  */
 public interface Admin {
